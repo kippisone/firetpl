0054523b093908ae01dbf18d95e837b771e3d7e6
Andi <andi@noname-media.com>
2014-06-13 12:58:59 +0200

    Change localePrecompiler module

--- modules/localePrecompiler.js
+++ modules/localePrecompiler.js
@@ -5,12 +5,17 @@ var extend = require('node.extend'),
 	FireTPL = require('../firetpl'),
 	glob = require('glob');
 
+console.log(typeof glob);
+
 module.exports = function() {
 	'use strict';
 
 	var LocalePrecompiler = function() {
 
 	};
+
+	//Makes glob better testable
+	LocalePrecompiler.prototype.glob = glob;
 
 	LocalePrecompiler.prototype.compile = function(options) {
 		options = extend({
@@ -19,22 +24,25 @@ module.exports = function() {
 		}, options);
 
 		this.verbose = options.verbose;
+		this.defaultLocale = options.defaultLocale || 'en-US';
 
 		if (this.verbose) {
 			console.log('Scan folder %s', options.baseDir);
 		}
 
-		var locales = this.parseFolder(options.baseDir);
+		this.parseFolder(options.baseDir, function(err, locales) {
+
+		});
 	};
 
-	LocalePrecompiler.prototype.parseFolder = function(dir) {
+	LocalePrecompiler.prototype.parseFolder = function(dir, callback) {
 		fs.exists(dir, function(state) {
 			var opts = {
 				cwd: dir,
 				stat: true
 			};
 
-			glob('**/*.*', opts, function(err, files) {
+			this.glob('**/*.*', opts, function(err, files) {
 				if (err) {
 					throw err;
 				}
@@ -43,12 +51,46 @@ module.exports = function() {
 					if (this.verbose) {
 						console.log(' >> parse locale %s', file);
 					}
-
-					
 				}.bind(this));
+
+				//Strip default locale
+				var defaultLocale,
+					locales = {};
+
+				for (var i = 0, len = files.length; i < len; i++) {
+					if (path.basename(files[i],'.json') === this.defaultLocale) {
+						defaultLocale = files.splice(i, 1);
+						defaultLocale = this.readFile(defaultLocale[0]);
+					}
+				}
+
+				locales[this.defaultLocale] = defaultLocale;
+				
+				//Strip other locales
+				for (i = 0, len = files.length; i < len; i++) {
+					var curLocale = path.basename(files[i],'.json');
+					if (/^[a-z]{2}-[A-Z]{2}$/.test(curLocale)) {
+						var source = files.splice(i, 1);
+						source = this.readFile(source[0]);
+						locales[curLocale] = extend({}, defaultLocale, source);
+					}
+				}
+
+
+				callback(null, locales);
 			}.bind(this));
 		}.bind(this));
 	};
+
+	LocalePrecompiler.prototype.readFile = function(file) {
+		var source = fs.readFileSync(path.join(this.baseDir, file, {encoding:'utf8'}));
+		if (path.extname(file) === '.json') {
+			source = JSON.parse(source);
+		}
+
+		return source;
+	};
+
 
 	return LocalePrecompiler;
 	
--- modules/tests/localePrecompiler.spec.js
+++ modules/tests/localePrecompiler.spec.js
@@ -1,7 +1,6 @@
 var fs = require('fs');
 
-var LocalePrecompiler = require('../localePrecompiler'),
-	glob = require('glob');
+var LocalePrecompiler = require('../localePrecompiler');
 
 describe('LocalePrecompiler', function() {
 	'use strict';
@@ -29,27 +28,44 @@ describe('LocalePrecompiler', function() {
 		var compiler,
 			fsExistsStub,
 			globStub,
-			__glob;
+			readFileStub;
 
 		beforeEach(function() {
-			fsExistsStub = sinon.stub(fs, 'exists');
-			globStub = sinon.stub();
-			__glob = glob;
-				
 			compiler = new LocalePrecompiler();
+			fsExistsStub = sinon.stub(fs, 'exists');
+			globStub = sinon.stub(compiler,'glob');
+			readFileStub = sinon.stub(compiler, 'readFile');
+			
 		});
 
 		afterEach(function() {
-			glob = __glob;
 			fsExistsStub.restore();
+			globStub.restore();
+			readFileStub.restore();
 		});
 		
-		it('Should compile a locale folder', function() {
+		it('Should compile a locale folder', function(done) {
 			fsExistsStub.yields(true);
-			globStub.yields(null, ['/home/test/project/locale/de-DE.json', '/home/test/project/locale/en-EN.json']);
+			globStub.yields(null, ['./locale/de-DE.json', './locale/en-EN.json']);
+			readFileStub.onFirstCall().returns({'greeding':'Hello World', 'char': 'a'});
+			readFileStub.onSecondCall().returns({'greeding':'Hallo Welt', 'letter': 'b'});
 
+			compiler.defaultLocale = 'en-EN';
+			compiler.parseFolder('test', function(err, locales) {
+				console.log(locales);
 
-			compiler.parseFolder();
+				expect(readFileStub).was.calledTwice();
+				expect(readFileStub).was.calledWith('./locale/en-EN.json');
+				expect(readFileStub).was.calledWith('./locale/de-DE.json');
+				
+				expect(locales).to.eql({
+					'en-EN': {'greeding': 'Hello World', 'char': 'a'},
+					'de-DE': {'greeding': 'Hallo Welt', 'char': 'a', 'letter': 'b'}
+				});
+
+				done();
+			});
+
 		});
 	});
 });
