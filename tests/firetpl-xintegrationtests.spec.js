e0a348d3c0d23b7276456225cc3d8496537aa9c1
Andi <andi@noname-media.com>
2014-06-28 09:24:43 +0200

    Add integration test

---
+++ tests/firetpl-xintegrationtests.spec.js
@@ -0,0 +1,17 @@
+describe('FireTPL integration tests', function() {
+    'use strict';
+    
+    describe('FireTPL.precompile()', function() {
+        beforeEach(function() {
+
+        });
+
+        afterEach(function() {
+
+        });
+
+        it('', function() {
+            //Test Content
+        });
+    });
+});
