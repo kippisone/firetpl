933bbadedcbf7a4611c761b7b70554861058f30f
AndiOxidant <andi.oxidant@noname-media.com>
2015-06-01 08:06:10 +0200

    Build

--- firetpl-runtime.js
+++ firetpl-runtime.js
@@ -1,5 +1,5 @@
 /*!
- * FireTPL template engine v0.6.0-12
+ * FireTPL template engine v0.6.0-13
  * 
  * FireTPL is a pretty Javascript template engine. FireTPL uses indention for scops and blocks, supports partials, helper and inline functions.
  *
@@ -53,7 +53,7 @@ var FireTPL;
          * @property {String} version
          * @default v0.6.0
          */
-        version: '0.6.0-12',
+        version: '0.6.0-13',
 
         /**
          * Defines the default language
